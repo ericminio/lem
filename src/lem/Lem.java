a58a40ecd2157d5873f72d7db48b1be5a2093a1b
testaddict <eric@macbook-pro-de-eric-mignot.local>
2010-05-29 06:22:10 +0200

    first commit

---
+++ src/lem/Lem.java
@@ -0,0 +1,15 @@
+package lem;
+
+public class Lem {
+
+	private int altitude;
+	
+	public void setAltitude(int newAltitude) {
+		altitude = newAltitude;
+	}
+
+	public int getAltitude() {
+		return altitude;
+	}
+
+}
---
+++ test/lem/LemBacklogTest.java
@@ -0,0 +1,25 @@
+package lem;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LemBacklogTest {
+
+	private Lem lem;
+	
+	@Before public void
+	init() {
+		lem = new Lem();
+	}
+	
+	@Test public void
+	canModifyAltitude() {
+		lem.setAltitude(50);
+		assertThat(lem.getAltitude(), equalTo(50));
+		lem.setAltitude(40);
+		assertThat(lem.getAltitude(), equalTo(40));
+	}
+}
